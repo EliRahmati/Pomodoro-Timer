a276d314da3f0e4a89907bdb05577e83f3e3e2f4
Elham Rahmati <elhamrahmati1986@gmail.com>
2023-05-03 23:33:20 +0200

    Added Button

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
-import {Button} from '@mui/material';
+import {Button, Paper, Box, Typography} from '@mui/material';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import { Alarm } from '@mui/icons-material';
 
 function Pomodoro(props) {
   const [timerId, setTimerId] = useState(null);
@@ -45,14 +47,29 @@ function Pomodoro(props) {
   }, [time])
 
   return (
-    <div>
-      <Button variant="contained" style={{margin: 5}} onClick={handlePomodo}>Pomodoro</Button>
-      <Button variant="contained" style={{margin: 5}} onClick={handleShortBreak}>Short Break</Button>
-      <Button variant="contained" style={{margin: 5}} onClick={handleLongBreak}>Long Break</Button>
-      <h1>Time: {Math.floor(time / 60)} min : {time % 60} seconds</h1>
-      <Button variant="contained" style={{margin: 5}} onClick={handleStartTimer}>Start</Button>
-      <Button variant="contained" style={{margin: 5}} onClick={handleStopTimer}>Stop</Button>
-    </div>
+    <React.Fragment>
+      <Paper variant="outlined" style={{ background: "darkblue", width: 1000, margin: "auto", opacity: [0.9, 0.8, 0.7]}} elevation={0}>
+        <Box margin={5} alignItems="center" justifyContent="center" display="flex">
+          <Alarm fontsize='small' style={{color: "#FFFFFF"}}></Alarm>
+          <h1 style={{color: "#FFFFFF"}}>Pomodoro Timer</h1>
+        </Box>
+      </Paper>
+      <Paper variant="outlined" style={{ background: "lightblue", width: 1000, margin: "auto", opacity: [0.9, 0.8, 0.7]}} elevation={0}>
+      <Box margin={5} alignItems="center" justifyContent="center" display="flex">
+        <Button variant="contained" style={{margin: 5}} onClick={handlePomodo}>Pomodoro</Button>
+        <Button variant="contained" style={{margin: 5}} onClick={handleShortBreak}>Short Break</Button>
+        <Button variant="contained" style={{margin: 5}} onClick={handleLongBreak}>Long Break</Button>
+        <Button variant="contained" style={{margin: 5}} onClick={handleLongBreak}>Reward</Button>
+      </Box>
+      <Box margin={"auto"} alignItems="center" justifyContent="center" display="flex">
+      <Typography variant="h4" style={{margin: 100}}>Time: {Math.floor(time / 60)} min : {time % 60} seconds</Typography>
+      </Box>
+      <Box margin={"auto"} alignItems="center" justifyContent="center" display="flex">
+        <Button variant="contained" style={{margin: 5}} onClick={handleStartTimer}>Start</Button>
+        <Button variant="contained" style={{margin: 5}} onClick={handleStopTimer}>Stop</Button>
+      </Box>
+    </Paper>
+    </React.Fragment>
   );
 }
 
